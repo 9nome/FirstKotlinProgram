2a69df93a0f77ac37fd59f61bfa22b2d9ef1cebf
Joshua Strickland <joshua.strickland@foresee.com>
2019-08-29 17:04:23 -0400

    First Kotlin Commit!

--- src/FirstClass.kt
+++ src/FirstClass.kt
@@ -1,10 +1,19 @@
 class FirstClass {
+    val iAmAnInteger: Int = 1
+    val iAmAnIntegerInDisguise = 1
+
     fun iAmAMethod() {
         println("wow such compact")
+    }
+
+    fun iHateMath(x: Int, y: Int){
+        println(x+y)
+        //return this //why dont work?! no this?!
     }
 }
 //Weird! Main cannot be in a class in Kotlin. Needs to be outside a class def
 fun main(){
     val firstClass = FirstClass()
     firstClass.iAmAMethod()
+    firstClass.iHateMath(firstClass.iAmAnInteger, firstClass.iAmAnIntegerInDisguise)
 }
